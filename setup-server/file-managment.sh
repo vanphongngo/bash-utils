cd3fff3e0224f467061c212bbf82a9a184d3e7fe
phongnv4 <phongnv4@vng.com.vn>
2025-10-14 11:07:35 +0700

    refactor

--- setup-server/file-managment.sh
+++ setup-server/file-managment.sh
@@ -1,2 +1,5 @@
 # set permission read/write for all uid to the data and folder/files insider the data; use for set permission for volume mount; suitable for dev/test; should set permission for specifc userid; it will be safer
 sudo chmod -R 1777 ./data
+
+# disk size check
+du -sh *
